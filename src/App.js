49a2d12f5ca7e8a2646cf1997842df75853e8c3c
Divyesh304 <savaliyadivyesh304@gmail.com>
2022-09-02 19:29:56 -0700

    first commi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
+ const [val,setval]=useState("")
+
+ const btnClick =(e)=>{
+   setval(val.concat(e.target.value))
+ } 
+ 
+ const clear=()=>{
+   setval("")
+ }
+
+ const back=()=>{
+  setval(val.slice(0,-1))
+ }
+
+ const cal=()=>{
+   setval (eval(val))
+ }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <Container>
+       <input type="text" className='cal' value={val}/>
+       
+       <br/><br/><br/>
+      <button value="1" onClick={btnClick}>1</button>
+      <button value="2" onClick={btnClick}>2</button>
+      <button value="3" onClick={btnClick}>3</button>
+      <br/><br/><br/>
+
+
+      <button value="4" onClick={btnClick}>4</button>
+      <button value="5" onClick={btnClick}>5</button>
+      <button value="6" onClick={btnClick}>6</button>
+      <br/><br/><br/>
+
+
+      <button value="7" onClick={btnClick}>7</button>
+      <button value="8" onClick={btnClick}>8</button>
+      <button value="9" onClick={btnClick}>9</button>
+      <br/><br/><br/>
+
+      <button value="+" onClick={cale}>+</button>
+      <button value="-" onClick={cale}>-</button>
+      <button value="*" onClick={cale}>*</button>
+      <button value="/" onClick={cale}>/</button>
+
+
+      <br/><br/><br/>
+      <button value="clear" onClick={clear}>Clear</button>
+      <button value="back" onClick={back}>back</button>
+
+      <button value="=" onClick={cal}>=</button>
+
+     </Container>
+
+    </>
   );
 }
 
